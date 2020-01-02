4279b8d988e0e6fe01f667bc8652c212e42b7058
Geetansh Monga <gmgog0402@gmail.com>
2020-01-02 16:15:07 +0530

    designing

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('',views.login_view,name='login_page'),
-    path('signup/',views.signup_view,name='signup_page'),
+    path('',views.login_signup_view,name='login_page'),
+    # path('signup/',views.signup_view,name='signup_page'),
     path('logout/',views.logout_view,name='logout_page'),
 ]
--- website/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import website_view
-
-app_name = 'website'
-urlpatterns = [
-    path('',website_view,name='main_website')
-]
