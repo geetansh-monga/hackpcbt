25914b3376a4ac2e3c86107e19b5c2bb1a0fd015
Geetansh Monga <gmgog0402@gmail.com>
2020-01-02 16:27:54 +0530

    changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('',views.login_signup_view,name='login_page'),
-    # path('signup/',views.signup_view,name='signup_page'),
     path('logout/',views.logout_view,name='logout_page'),
 ]
